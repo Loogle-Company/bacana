00f8a2fa1cef0a5155d2450e7cf0da41fc24a264
louglecompany <144390491+louglecompany@users.noreply.github.com>
2023-11-22 11:15:15 -0300

    Update script.js

--- escolha/flip-two/js/script.js
+++ escolha/flip-two/js/script.js
@@ -24,6 +24,19 @@
     const toggleButton2 = document.getElementById("toggleButton2");
     const toggleButton3 = document.getElementById("toggleButton3");
     const toggleButton4 = document.getElementById("toggleButton4");
+    const toggleButton1 = document.getElementById("toggleButton5");
+    const toggleButton2 = document.getElementById("toggleButton6");
+    const toggleButton3 = document.getElementById("toggleButton7");
+    const toggleButton4 = document.getElementById("toggleButton8");
+    const toggleButton1 = document.getElementById("toggleButton9");
+    const toggleButton2 = document.getElementById("toggleButton10");
+    const toggleButton3 = document.getElementById("toggleButton11");
+    const toggleButton4 = document.getElementById("toggleButton12");
+    const toggleButton1 = document.getElementById("toggleButton13");
+    const toggleButton2 = document.getElementById("toggleButton14");
+    const toggleButton3 = document.getElementById("toggleButton15");
+    const toggleButton4 = document.getElementById("toggleButton16");
+    const toggleButton1 = document.getElementById("toggleButton17");
     const prevButton = document.getElementById("prevButton");
     const nextButton = document.getElementById("nextButton");
 
@@ -31,7 +44,20 @@
         document.getElementById("card1"),
         document.getElementById("card2"),
         document.getElementById("card3"),
-        document.getElementById("card4")
+        document.getElementById("card4"),
+        document.getElementById("card5"),
+        document.getElementById("card6"),
+        document.getElementById("card7"),
+        document.getElementById("card8"),
+        document.getElementById("card9"),
+        document.getElementById("card10"),
+        document.getElementById("card11"),
+        document.getElementById("card12"),
+        document.getElementById("card13"),
+        document.getElementById("card14"),
+        document.getElementById("card15"),
+        document.getElementById("card16")
+        document.getElementById("card17")
     ];
     let currentIndex = 0;
 
@@ -50,6 +76,59 @@
     toggleButton4.addEventListener("click", function () {
         showCard(3);
     });
+
+    toggleButton1.addEventListener("click", function () {
+        showCard(4);
+    });
+
+    toggleButton2.addEventListener("click", function () {
+        showCard(5);
+    });
+
+    toggleButton3.addEventListener("click", function () {
+        showCard(6);
+    });
+
+    toggleButton4.addEventListener("click", function () {
+        showCard(7);
+    });
+
+    toggleButton1.addEventListener("click", function () {
+        showCard(8);
+    });
+
+    toggleButton2.addEventListener("click", function () {
+        showCard(9);
+    });
+
+    toggleButton3.addEventListener("click", function () {
+        showCard(10);
+    });
+
+    toggleButton4.addEventListener("click", function () {
+        showCard(11);
+    });
+
+    toggleButton1.addEventListener("click", function () {
+        showCard(12);
+    });
+
+    toggleButton2.addEventListener("click", function () {
+        showCard(13);
+    });
+
+    toggleButton3.addEventListener("click", function () {
+        showCard(14);
+    });
+
+    toggleButton4.addEventListener("click", function () {
+        showCard(15);
+    });
+
+    toggleButton1.addEventListener("click", function () {
+        showCard(16);
+    });
+
 
     prevButton.addEventListener("click", function () {
         currentIndex = (currentIndex - 1 + cards.length) % cards.length;
